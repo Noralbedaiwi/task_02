b0c3041a4b3e409a964a2bf1b8b083c405c51dea
Nora <n.albedaiwi@hotmail.com>
2018-12-02 16:09:20 +0300

    submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def coded(request):
+	context = {
+		"msg" : "Hello World!"
+	}
+
+	return render(request, 'bootcamp.html', context)
